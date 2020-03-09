8032804b29d909529142f492811434bf629316de
Richie Bendall <richiebendall@gmail.com>
2020-03-09 21:58:56 +1300

    refactor: Use in-house implementation
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 "use strict"
 
-const syncPromise = require("synchronized-promise")
+const deasync = require("deasync")
 const isPromise = require("p-is-promise")
-
-// Suppress DEP0097
-process.noDeprecation = true
 
 module.exports = (promise) => {
 	if (typeof promise === "function") promise = promise()
 	if (!isPromise(promise)) throw new TypeError("A promise must be provided!")
 
-	return syncPromise(() => promise)()
+	let resolved = false
+	let resolvedData
+
+	let thrown = false
+	let thrownError
+
+	promise
+		.then((data) => {
+			resolvedData = data
+			resolved = true
+		})
+		.catch((error) => {
+			thrownError = error
+			thrown = true
+		})
+
+	deasync.loopWhile(() => !resolved && !thrown)
+
+	if (thrown) throw thrownError
+
+	if (resolved) return resolvedData
 }
